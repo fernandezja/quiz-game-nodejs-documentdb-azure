f6d6dfee600b0be7a4319932c961ca8fdcd0fdfa
Jose A. Fernandez <fernandezja@gmail.com>
2016-04-26 13:33:33 -0300

    Version preliminar. Vista home/main. Preguntas dentro de Partida

---
+++ QuizGame/routes/index.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.render('home', { title: 'QuizGame'});
+});
+
+router.get('/main', function (req, res) {
+    res.render('index', { title: '' });
+});
+
+module.exports = router;
---
+++ QuizGame/routes/pregunta.js
@@ -0,0 +1,45 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/partida/:partidaId/pregunta/:preguntaId', function (req, res) {
+    
+    var partidaId = req.params.partidaId;
+    var preguntadId = req.params.preguntaId;
+    console.log('partidaId:', partidaId);
+    console.log('preguntadId:', preguntadId);
+    
+
+    var pregunta = {
+        id : preguntadId,
+        descripcion: '¿Cual es el color favorito?',
+        respuestas: [
+            {
+                id: 1,
+                descripcion: 'Rta 1',
+                esCorrecta: false,
+            },
+            {
+                id: 2,
+                descripcion: 'Rta 2',
+                esCorrecta: true,
+            },
+            {
+                id: 3,
+                descripcion: 'Rta 3',
+                esCorrecta: false,
+            }
+        ]
+    }
+    
+    
+    var preguntaVm = {
+        partida: { id: 8 },
+        pregunta: pregunta
+    };
+    
+
+    res.render('pregunta', preguntaVm);
+});
+
+module.exports = router;
