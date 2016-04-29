102fa65818b7bf5ba2ae342b6c41a09d0a9109e7
Jose A. Fernandez <fernandezja@gmail.com>
2016-04-29 15:23:24 -0300

    Eliminacion de archivo config.js, mejoras en nombre de app en minusculas

--- QuizGame/config.js
+++
@@ -1,8 +0,0 @@
-﻿var config = {}
-
-config.host = process.env.HOST || "[the URI value from the DocumentDB Keys blade on http://portal.azure.com]";
-config.authKey = process.env.AUTH_KEY || "[the PRIMARY KEY value from the DocumentDB Keys blade on http://portal.azure.com]";
-config.databaseId = "QuizGameDB";
-config.collectionId = "Partidas";
-
-module.exports = config;
