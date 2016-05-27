c062b2b1c2269b49371ec69ac21b050b4714961f
Jose A. Fernandez <fernandezja@gmail.com>
2016-05-27 15:19:17 -0300

    Partida con primer pregunta desde DocumentDB

--- QuizGame/routes/partida.js
+++ QuizGame/routes/partida.js
@@ -5,14 +5,20 @@ var config = require('../config');
 var DocumentDBClient = require('documentdb').DocumentClient;
 var RepositorioBase = require('../datos/repositoryBase.js');
 var PartidaRepository = require('../datos/partidaRepository.js');
-
+var PreguntaRepository = require('../datos/preguntaRepository.js');
 
 var docDbClient = new DocumentDBClient(config.host, {
     masterKey: config.authKey
 });
-var repositoryBase = new RepositorioBase(docDbClient, config.databaseId, config.collectionId);
-var partidaRepository = new PartidaRepository(repositoryBase);
-repositoryBase.init();
+
+var repositoryBaseParaPartida = new RepositorioBase(docDbClient, config.databaseId, config.collectionId);
+var partidaRepository = new PartidaRepository(repositoryBaseParaPartida);
+repositoryBaseParaPartida.init();
+
+var repositoryBaseParaPregunta = new RepositorioBase(docDbClient, config.databaseId, config.collectionPreguntas);
+var preguntaRepository = new PreguntaRepository(repositoryBaseParaPregunta);
+repositoryBaseParaPregunta.init();
+
 
 router.get('/listado', function (req, res) {
     
@@ -99,56 +105,53 @@ router.get('/:partidaId/pregunta/:preguntaId', function (req, res) {
     console.log('preguntadId:', preguntadId);
     console.log('respuestaId:', respuestaId);
     
-    var pregunta = {
-        id : preguntadId,
-        descripcion: '¿Cual es el color favorito?',
-        respuestas: [
-            {
-                id: 1,
-                descripcion: 'Rta 1',
-                esCorrecta: false,
-                css: 'no'
-            },
-            {
-                id: 2,
-                descripcion: 'Rta 2',
-                esCorrecta: true,
-                css: 'si'
-            },
-            {
-                id: 3,
-                descripcion: 'Rta 3',
-                esCorrecta: false,
-                css: 'no'
-            }
-        ]
-    }
     
-    for (var i = 0; i < pregunta.respuestas.length; i++) {
-        var r = pregunta.respuestas[i];
-        if (r.id == respuestaId) {
-            if (r.esCorrecta) {
-                r.css = 'btn-success';
-            } else {
-                r.css = 'btn-danger';
-            }
-            
+    var partidaPreguntaVm = {
+        partida: { id: partidaId },
+        preguntaActual: null,
+        preguntas: [],
+        respuestaId: respuestaId,
+        tieneRespuesta: (respuestaId != undefined),
+        mensaje:''
+    };
+
+    
+    //Consultando al repository
+    preguntaRepository.listado(function (items) {
+        if (items != null) {
+            partidaPreguntaVm.preguntas = items;
         } else {
-            r.css = '';
+            partidaPreguntaVm.mensaje = 'Sin partidas generadas'
         }
         
-    };
+        
+        partidaPreguntaVm.preguntaActual = partidaPreguntaVm.preguntas[0];
+        
+        for (var i = 0; i < partidaPreguntaVm.preguntaActual.respuestas.length; i++) {
+            var r = partidaPreguntaVm.preguntaActual.respuestas[i];
+            
+            //fix para respuestas sin id definido
+            if (r.id===undefined) {
+                r.id = i;
+            }
+
+            if (r.id == respuestaId) {
+                if (r.esCorrecta) {
+                    r.css = 'btn-success';
+                } else {
+                    r.css = 'btn-danger';
+                }
+            
+            } else {
+                r.css = '';
+            }
+        
+        };
+        
+        res.render('partidaPregunta', partidaPreguntaVm);
+    });
     
     
-    var preguntaVm = {
-        partida: { id: partidaId },
-        pregunta: pregunta,
-        respuestaId: respuestaId,
-        tieneRespuesta: (respuestaId != undefined)
-    };
-    
-    
-    res.render('pregunta', preguntaVm);
 });
 
 
