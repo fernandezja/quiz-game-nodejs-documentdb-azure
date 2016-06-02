b6dabc550a9a538e0251975d3590efb7ebfe7ade
MLEnzo <programasdeenzo@gmail.com>
2016-06-02 17:48:02 -0300

    se agrega api's dashboart de totales, jugadas, y en api de partida se agrega el de partidas jugadas....y por ultimo se agrega el view del dashboart

---
+++ QuizGame/routesApi/dashBoardApi.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/:dashboart/totales', function (req, res) {
+
+    //ruta para api/dashboart/totales
+
+
+
+
+
+
+    res.json();
+});
+
+
+
+module.exports = router;
---
+++ QuizGame/routesApi/jugadasApi.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/:jugadas/', function (req, res) {
+
+    //ruta para api/jugadas/fechaInicio/fechaTope
+
+
+
+
+
+
+    res.json();
+});
+
+
+
+module.exports = router;
