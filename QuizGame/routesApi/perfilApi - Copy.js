0d280d296406203b452c47bdfbf21e7eddba79bb
cristian <cristiangimenez1869@gmail.com>
2016-06-03 16:20:27 -0300

    Eliminar pregunta

--- QuizGame/routesApi/perfilApi - Copy.js
+++ QuizGame/routesApi/perfilApi - Copy.js
@@ -3,18 +3,14 @@ var router = express.Router();
 
 router.get('/:perfil/', function (req, res) {
     
-    var perfildId = req.params.perfilId;
+    var perfilId = req.params.perfilId;
     
     var perfil = {
-        id : perfildId,
-        nombre: 'Cristian',
-        apellido: 'Gimenez',
-        imagenUrl: 'foto de perfil',
-        puntaje: '2000',
-        preguntasTotales: '100',
-        preguntasCorrectas: '5',
-        preguntasIncorrectas: ''
-
+        id : perfilId,
+        nombre: 'Cristhian',
+        apellido: 'Chocron',
+        imagenUrl: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xlp1/v/t1.0-1/p200x200/10425355_10203649454730210_3179339780669292461_n.jpg?oh=e0ec0015be663600c4c4d9685138efcc&oe=57A4436F&__gda__=1473840083_2b8c9f95098f35878f822cb3493f6c9d',
+        
     }
     
     
