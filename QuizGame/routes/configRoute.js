d2f8795e1abe1c0313668e62810a54414a208ec3
Jose A. Fernandez <fernandezja@gmail.com>
2016-06-14 16:04:07 -0300

    Verificacion esAdmin para visualizar configuracion

--- QuizGame/routes/configRoute.js
+++ QuizGame/routes/configRoute.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var config = require('../config');
 
 router.get('/general', function (req, res) {
+
+    if (!res.locals.esAdmin) res.redirect(401, '/sinpermiso');
+
     res.render('configGeneral', { config: config });
 });
 
